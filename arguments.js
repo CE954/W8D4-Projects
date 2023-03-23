2f9bb5ecb2773c3d09c295db749f6e45c104c837
Christian <christianday954@ymail.com>
2023-03-23 15:14:24 -0700

    finish bind

--- arguments.js
+++ arguments.js
@@ -15,6 +15,38 @@ function sum (...args) {
 }
 
 
-console.log(sum(1, 2, 3, 4))
+// console.log(sum(1, 2, 3, 4))
 
+Function.prototype.myBind = function (ctx, ...args1) {
+    // let args2 = Array.from(arguments).slice(1);
+    // console.log(args2)
+    let that = this; 
+    return function(...args2) {
+        // let args = Array.from(arguments)
+        that.apply(ctx, [...args1, ...args2]);
+    }
+}
 
+class Cat {
+    constructor(name) {
+        this.name = name;
+    }
+
+    says(sound, person) {
+        console.log(`${this.name} says ${sound} to ${person}!`);
+        return true;
+    }
+}
+
+class Dog {
+    constructor(name) {
+        this.name = name;
+    }
+}
+
+const markov = new Cat("Markov");
+const pavlov = new Dog("Pavlov");
+
+markov.says.myBind(pavlov, "meow")("Markov");
+// Pavlov says meow to Markov!
+// true
